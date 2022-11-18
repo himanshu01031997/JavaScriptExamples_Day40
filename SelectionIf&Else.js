bf185ba81cfc75cc9ff4933f63f76a330798dcbc
himanshu01031997 <hs010397@gmail.com>
2022-11-18 23:48:53 +0530

    add:leap year

--- SelectionIf&Else.js
+++ SelectionIf&Else.js
@@ -1,30 +1,44 @@
-/* ========(use case1: greates number)=============*/
-let max=999;
-let min=100;
- let number1=Math.floor(Math.random() * (max - min + 1) + min);
- let number2=Math.floor(Math.random() * (max - min + 1) + min);
- let number3=Math.floor(Math.random() * (max - min + 1) + min);
+// /* ========(use case1: greates number)=============*/
+// let max=999;
+// let min=100;
+//  let number1=Math.floor(Math.random() * (max - min + 1) + min);
+//  let number2=Math.floor(Math.random() * (max - min + 1) + min);
+//  let number3=Math.floor(Math.random() * (max - min + 1) + min);
 
-  console.log(number1,number2,number3);
-  if (number1 > number2)
-  {
-      if(number1 > number3)
-      {
-          console.log("First number is largest among all");
-      }
-      else
-      {
-          console.log("Third number is largest among all");
-      }
-  }
-  else if (number2 > number3)
-  {
-      console.log("the Second numbers is largest among all");
-  }
-  else
-  {
-      console.log("the Third number is greatest among all");
-  }
+//   console.log(number1,number2,number3);
+//   if (number1 > number2)
+//   {
+//       if(number1 > number3)
+//       {
+//           console.log("First number is largest among all");
+//       }
+//       else
+//       {
+//           console.log("Third number is largest among all");
+//       }
+//   }
+//   else if (number2 > number3)
+//   {
+//       console.log("the Second numbers is largest among all");
+//   }
+//   else
+//   {
+//       console.log("the Third number is greatest among all");
+//   }
 
+/*==============(use case2 leap year)================ */
 
+let year=1980;
+if (year % 400 == 0)
+{
+  console.log("year is leap year");
+}
+else if (year % 4 == 0)
+{
+  console.log("year is leap year");
+}
+else
+{
+  console.log("year is not leap year");
+}
 
